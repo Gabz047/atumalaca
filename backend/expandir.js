607375487528ffa06070706c3396406729ead095
Gabz047 <gabe1612mail@gmail.com>
2023-08-25 00:30:44 -0300

    att8.0

--- backend/expandir.js
+++ backend/expandir.js
@@ -1,6 +1,12 @@
 var btnExp = document.querySelector('#btn-exp')
 var menuSide = document.querySelector('.menu-lateral')
+var categoria = document.querySelector('#categorias')
+var drop = document.querySelector('.dropdown-rsp')
 
 btnExp.addEventListener('click', function(){
     menuSide.classList.toggle('expandir')
+})
+
+categoria.addEventListener('click', function(){
+   drop.classList.toggle('descer')
 })
